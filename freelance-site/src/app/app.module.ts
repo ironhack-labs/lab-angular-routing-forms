423cb45b0f7a090a6a6d05a26d7858ca2f5840d3
asm-dev <salamanca.m@outlook.com>
2022-11-30 20:33:24 +0100

    add material form fields

--- freelance-site/src/app/app.module.ts
+++ freelance-site/src/app/app.module.ts
@@ -9,9 +9,12 @@ import { ErrorComponent } from './components/error/error.component';
 import { ProjectComponent } from './components/project/project.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from "@angular/material/input";
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
@@ -30,6 +33,10 @@ import { MatCardModule } from '@angular/material/card';
     ReactiveFormsModule,
     MatButtonModule,
     MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
