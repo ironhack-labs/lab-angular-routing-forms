6e3b766b1b26bddc2107d2c449dc8df60d28a177
Blanca <blancabcbt@gmail.com>
2022-11-30 18:15:26 +0100

    add styles to form

--- angular-router-project/src/app/app.module.ts
+++ angular-router-project/src/app/app.module.ts
@@ -8,6 +8,12 @@ import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
 
 
 
@@ -25,7 +31,11 @@ import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.co
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-   
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    
 
     
   ],
