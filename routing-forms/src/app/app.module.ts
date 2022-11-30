2b175276b39293936a16a1243654f80119af521c
Lenis Cervantes <leniserik@gmail.com>
2022-11-30 19:35:34 +0100

    form not working

--- routing-forms/src/app/app.module.ts
+++ routing-forms/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { ContactpageComponent } from './components/contactpage/contactpage.component';
 import { ErrorpageComponent } from './components/errorpage/errorpage.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- routing-forms/src/app/components/contactpage/contactpage.component.ts
+++ routing-forms/src/app/components/contactpage/contactpage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-contactpage',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./contactpage.component.css']
 })
 export class ContactpageComponent {
+  registerForm: FormGroup;
+  nameInput: FormControl;
+  emailInput: FormControl;
+  occupationInput: FormControl;
+  subjectInput: FormControl;
+  contentInput: FormControl
 
+  // It will have the fields name, occupation, email, subject, and content.
+  // Every field is required but occupation.
+  // Content must be a textarea field and has a minimum length of 10 and a maximum length of 255 characters.
+  // You must highlight all invalid fields and show error messages.
+  // The email must be a valid email address.
+  // After successful submission, you must show a success message and clear the form.
+  constructor() {
+    this.nameInput = new FormControl('', [
+      Validators.required,
+    ]);
+    this.subjectInput = new FormControl('', [
+      Validators.required,
+    ]);
+    this.occupationInput = new FormControl('', [
+      Validators.minLength(10),
+      Validators.maxLength(255),
+    ])
+    this.emailInput = new FormControl('', [
+      Validators.required,
+      Validators.email
+    ])
+    this.contentInput = new FormControl('', [
+      Validators.required,
+    ])
+
+    this.registerForm = new FormGroup({
+      name: this.nameInput,
+      email: this.emailInput,
+      occupationInput: this.occupationInput,
+      subjectInput: this.subjectInput,
+      contentInput: this.contentInput
+    });
+  }
+
+
+  onSubmit(): void {
+    console.log('User created...');
+    this.nameInput = new FormControl('', []);
+  }
 }
