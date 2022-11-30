71b86567aa2b08e926c9fbf99bc1f08baa7f7362
Blanca <blancabcbt@gmail.com>
2022-11-30 16:44:15 +0100

    validators working

--- angular-router-project/src/app/app.module.ts
+++ angular-router-project/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
-import { RouterModule, Routes } from "@angular/router";
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
--- angular-router-project/src/app/components/contact/contact.component.ts
+++ angular-router-project/src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component ({
   selector: 'app-contact',
@@ -6,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+
+  contactForm: FormGroup;
+  nameInput: FormControl;
+  occupationInput: FormControl;
+  emailInput: FormControl;
+  subjectInput: FormControl;
+  contentInput: FormControl;
+
+  constructor(){
+    this.nameInput = new FormControl("", Validators.required);
+    this.occupationInput = new FormControl("");
+    this.emailInput = new FormControl("", [Validators.required, Validators.email]);
+    this.subjectInput = new FormControl("", Validators.required);
+    this.contentInput = new FormControl("",[Validators.required, Validators.minLength(10), Validators.maxLength(255)]);
+
+    this.contactForm = new FormGroup({
+      name: this.nameInput,
+      occupation: this.occupationInput,
+      email: this.emailInput,
+      subject: this.subjectInput,
+      content: this.contentInput
+    })
+  }
+
+
   ngOnInit(): void {}
 
   onSubmit(): void{
