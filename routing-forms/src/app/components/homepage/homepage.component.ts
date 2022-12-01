5ba02d1722a5db21cc342d2834690ea2570206f0
Lenis Cervantes <leniserik@gmail.com>
2022-12-01 21:31:17 +0100

    All done

--- routing-forms/src/app/components/homepage/homepage.component.ts
+++ routing-forms/src/app/components/homepage/homepage.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent {
+  prj1: boolean
+  prj2: boolean
+  prj3: boolean
+  project: any
+  constructor() {
+    this.prj1 = false
+    this.prj2 = false
+    this.prj3 = false
+    this.project = document.querySelector('bottom')
+  }
 
+  showPrj1() {
+    this.prj1 = true
+    this.prj2 = false
+    this.prj3 = false
+  }
+  showPrj2() {
+    this.prj2 = true
+    this.prj1 = false
+    this.prj3 = false
+  }
+  showPrj3() {
+    this.prj3 = true
+    this.prj2 = false
+    this.prj1 = false
+  }
 }
