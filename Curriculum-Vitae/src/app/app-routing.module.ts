ac728ae1ce8cdab2b2a3802cb54bace7e69b8efa
Daniela Cima <trinchera.dm@gmail.com>
2022-12-02 15:56:22 +0100

    nav-bar & routing

---
+++ Curriculum-Vitae/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: '', // Default home page
+    component: HomePageComponent,
+  },
+  {
+    path: 'contact', // URL with params
+    component: ContactPageComponent,
+  },
+  {
+    path: '**', // 404 Page
+    component: PageNotFoundComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ Curriculum-Vitae/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    ContactPageComponent,
+    PageNotFoundComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
