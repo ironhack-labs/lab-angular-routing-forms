0bb1a08902243e2af4b25995af311e76af28ed71
IreneOliveto <nene.ol@hotmail.it>
2022-12-02 14:49:38 +0100

    first commit

--- routing-lab/src/app/app.module.ts
+++ routing-lab/src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { ContactpageComponent } from './components/contactpage/contactpage.component';
@@ -14,11 +21,19 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     HomepageComponent,
     ContactpageComponent,
     ErrorpageComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- routing-lab/src/app/components/homepage/homepage.component.ts
+++ routing-lab/src/app/components/homepage/homepage.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+}
+
+
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
